04972745a6ce05a170a6283e5996e4179f7764d5
niku bisht <akbisht1973.sundari@gmail.com>
2020-11-04 14:42:04 +0530

    make it better

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const Nexmo = require('nexmo');
 const path = require('path');
-
+const bodyParser = require('body-parser');
 const PORT =process.env.PORT || 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/',(req,res)=>{
 console.log(`sent`);
@@ -28,6 +30,25 @@ app.get('/index',(req,res)=>{
     res.sendFile(path.join(__dirname , './public','index.html'));
 
 });
+
+app.post('/accept',(req,res)=>{
+
+const {num,message}  = req.body;
+const nexmo = new Nexmo({
+    apiKey:'d8204f2d',
+    apiSecret:'6WIpSt4CiqR29yov'
+});
+
+console.info(`sent`)
+const from = 'Vonage APIs';
+const to = num.toString();
+const text = message;
+
+nexmo.message.sendSms(from,to,text);
+
+
+});
+
 app.listen(PORT,()=>{
 
 console.log(`Connected to port ${PORT}`);
