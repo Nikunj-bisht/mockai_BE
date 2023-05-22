1fceba78baf15ef3ad7e75f507ad88a2570200fe
Nikunj Bisht <nikkubisht12@gmail.com>
2023-05-22 23:18:38 +0530

    Nikunj | 1)folder structure

---
+++ models/jobCategories/Fields.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const fieldschema = new mongoose.Schema({
+
+title:{
+    type:String
+},
+
+description:{
+    type:String
+}
+
+
+
+
+})
+
+
+const Fieldmodel = mongoose.model("Fields" , fieldschema);
+
+module.exports = Fieldmodel;
+
--- scr.js
+++
@@ -1,8 +0,0 @@
-const sur = require('./survey');
-
-module.exports = async function call(){
-
-
-await sur.create({name:'counter',counter:0});
-
-}
